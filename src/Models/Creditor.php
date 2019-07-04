eccd7fcf5380d2a06e0f9ef3f86ef71b6d2b0eb5
sureyee <yixu@sureyee.com>
2019-07-04 16:44:26 +0800

    完善creditor模型

--- src/Models/Creditor.php
+++ src/Models/Creditor.php
@@ -2,14 +2,28 @@
 
 namespace Sureyee\LaravelIfcert\Models;
 
+use Sureyee\LaravelIfcert\Client;
 use Sureyee\LaravelIfcert\Contracts\IfcertModel;
 use Sureyee\LaravelIfcert\Contracts\Request;
 
-
+/**
+ * Class Creditor
+ * @package Sureyee\LaravelIfcert\Models
+ * @property $fin_claim_id
+ * @property $source_product_code
+ * @property $user_idcard_hash
+ * @property $inv_amount
+ * @property $inv_rate
+ * @property $inv_time
+ * @property $red_package
+ * @property $lock_time
+ */
 class Creditor extends IfcertModel
 {
 
     protected $table = 'ifcert_creditor';
+
+    protected $dates = ['inv_time', 'lock_time'];
 
     public static function getInfType()
     {
@@ -22,15 +36,52 @@ class Creditor extends IfcertModel
      */
     public function storeFromData(array $data, RequestLog $log)
     {
+        $this->fin_claim_id = $data['finClaimId'];
+        $this->source_product_code = $data['sourceProductCode'];
+        $this->user_idcard_hash = $data['userIdcardHash'];
+        $this->inv_time = $data['invAmount'];
+        $this->inv_rate = $data['invRate'];
+        $this->inv_time = $data['invTime'];
+        $this->red_package = $data['redpackage'];
+        $this->lock_time = $data['lockTime'];
+        $this->request_id = $log->id;
 
+        $this->save();
     }
 
     /**
      * 将数据库数据转化为上报数据结构
-     * @return array
      */
     public static function getTransformer()
     {
-        // TODO: Implement getTransformer() method.
+        return function (Creditor $creditor) {
+            return [
+                'version' => Client::version(),
+                'sourceCode' => $creditor->getSourceCode(),
+                'finClaimId' => $creditor->fin_claim_id,
+                'sourceProductCode' => $creditor->source_product_code,
+                'userIdcardHash' => $creditor->user_idcard_hash,
+                'invAmount' => $creditor->inv_amount,
+                'invRate' => $creditor->inv_rate,
+                'invTime' => $creditor->inv_time->format('Y-m-d H:i:s'),
+                'redpackage' => $creditor->red_package,
+                'lockTime' =>  $creditor->lock_time->format('Y-m-d'),
+            ];
+        };
+    }
+
+    public function getRedPackageAttribute($value)
+    {
+        return sprintf('%01.2f', $value);
+    }
+
+    public function getInvAmountAttribute($value)
+    {
+        return sprintf('%01.2f', $value);
+    }
+
+    public function getInvRateAttribute($value)
+    {
+        return sprintf('%01.6f', $value);
     }
 }
--- src/database/migrations/2019_06_20_000000_create_ifcert_creditor_table.php
+++ src/database/migrations/2019_06_20_000000_create_ifcert_creditor_table.php
@@ -12,7 +12,7 @@ class CreateIfcertCreditorTable extends Migration
             $table->unsignedInteger('request_id')->nullable()->comment('请求id');
             $table->string('fin_claim_id')->comment('初始债权编号');
             $table->string('source_product_code')->comment('散标信息编号');
-            $table->string('user_id_hash')->comment('出借用户证件号hash值');
+            $table->string('user_idcard_hash')->comment('出借用户证件号hash值');
             $table->float('inv_amount')->comment('出借金额(元)');
             $table->float('inv_rate', 7, 6)->comment('出借预期年化利率');
             $table->dateTime('inv_time')->comment('出借计息时间');
